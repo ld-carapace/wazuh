e94ce7668fd3a8c4374ad33b17b849d2f8ddcbea
Nico Stefani <nicolas.stefi@wazuh.com>
2024-09-08 10:48:16 -0300

    Fixed and skiped unit tests

--- framework/wazuh/core/indexer/tests/test_utils.py
+++ framework/wazuh/core/indexer/tests/test_utils.py
@@ -6,9 +6,9 @@ from wazuh.core.indexer.base import IndexerKey
 @pytest.fixture
 def search_result():
     return {IndexerKey.HITS: {IndexerKey.HITS: [
-        {IndexerKey._SOURCE: {IndexerKey._ID: 1}},
-        {IndexerKey._SOURCE: {IndexerKey._ID: 2}},
-        {IndexerKey._SOURCE: {IndexerKey._ID: 3}}
+        {IndexerKey._SOURCE: {IndexerKey.ID: 1}},
+        {IndexerKey._SOURCE: {IndexerKey.ID: 2}},
+        {IndexerKey._SOURCE: {IndexerKey.ID: 3}}
     ]}}
 
 
@@ -16,7 +16,7 @@ def test_get_source_items(search_result: dict):
     """Check the correct function of `get_source_items`."""
     output = [item for item in utils.get_source_items(search_result)]
 
-    assert output == [{IndexerKey._ID: 1}, {IndexerKey._ID: 2}, {IndexerKey._ID: 3}]
+    assert output == [{IndexerKey.ID: 1}, {IndexerKey.ID: 2}, {IndexerKey.ID: 3}]
 
 
 def test_get_source_items_id(search_result: dict):
